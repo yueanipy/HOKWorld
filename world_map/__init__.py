83b8bdaf3fc7768e0984000a10a7d5a16367a2a6
yueanipy <114322093+yueanipy@users.noreply.github.com>
2026-07-24 16:24:48 +0800

    新增公共地图入口

---
+++ world_map/__init__.py
@@ -0,0 +1,25 @@
+'公共地上地图接口。'
+
+from .core import (
+    MapLocation,
+    MapTarget,
+    MapTargetKind,
+    MapTargetStatus,
+    TargetObservation,
+    WorldMapAtlas,
+    WorldMapNavigator,
+    focus_source_coordinate,
+    teleport_to,
+)
+
+__all__ = (
+    "MapLocation",
+    "MapTarget",
+    "MapTargetKind",
+    "MapTargetStatus",
+    "TargetObservation",
+    "WorldMapAtlas",
+    "WorldMapNavigator",
+    "focus_source_coordinate",
+    "teleport_to",
+)
